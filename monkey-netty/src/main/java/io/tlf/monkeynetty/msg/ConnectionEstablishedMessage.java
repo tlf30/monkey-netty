81b72cfc1cd438c8a9be294980dde50b37e43679
Trevor Flynn <trevorflynn@liquidcrystalstudios.com>
2021-01-14 08:35:24 -0900

    Added some javadoc on ConnectionEstablishedMessage

--- monkey-netty/src/main/java/io/tlf/monkeynetty/msg/ConnectionEstablishedMessage.java
+++ monkey-netty/src/main/java/io/tlf/monkeynetty/msg/ConnectionEstablishedMessage.java
@@ -2,6 +2,12 @@ package io.tlf.monkeynetty.msg;
 
 import io.tlf.monkeynetty.NetworkProtocol;
 
+/**
+ * @author Trevor Flynn trevorflynn@liquidcrystalstudios.com
+ *
+ * This message is sent from the server to the client to nifty the
+ * client that the server is ready to receive messages.
+ */
 public class ConnectionEstablishedMessage implements NetworkMessage {
 
     @Override
